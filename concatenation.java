166642a060686727cf72e8bda73a4ac096892459
Charu2474 <145651542+Charu2474@users.noreply.github.com>
2024-07-29 14:25:56 +0530

    Create concatenation.java

---
+++ concatenation.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner f=new Scanner(System.in);
+        String s1=f.nextLine();
+        String s2=f.nextLine();
+        String s3="";
+        s3=s1+s2;
+        System.out.println("Friendship Rekindled! The concatenated string is "+s3+".");
+    }
+}
